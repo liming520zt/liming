98fdec4b7396db06d06cd28c5cf1ed7daa7e7445
liming <2271024172@qq.com>
2021-03-29 19:35:45 +0800

    李明第七次修改

--- src/com/Test.java
+++ src/com/Test.java
@@ -5,14 +5,4 @@ public class Test {
     private String name;
     //年龄
     private String age;
-    //增加性别属性
-    private String sex;
-    //增加住址
-    private String address;
-    //工资
-    private String salary;
-    //老板
-    private String boss;
-    //岗位
-    private String gangwei;
 }
